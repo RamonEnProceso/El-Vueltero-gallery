c0ca3e7fae30478facc0c5b52356453acde0150c
RamonEnProceso <ramon.en.proceso@gmail.com>
2025-12-12 16:09:56 -0300

    style: Agregar equinas decorativas

--- app/components/section/landing/landing.tsx
+++ app/components/section/landing/landing.tsx
@@ -15,14 +15,56 @@ const Landing = () =>{
             <JudithCanvas></JudithCanvas>
         </div>
         <div className="newspaper_back">
-                <Image src={"/assets/landing/1.webp"} 
+                <Image src={"/assets/landing/newspaper_1.webp"} 
                 fill
                 alt=""
                 loading="eager"
                 ></Image>
         </div>
-        <div className="newspaper_back">
-                <Image src={"/assets/landing/2.webp"} 
+        <div className="newspaper_back second">
+                <Image src={"/assets/landing/tapes.webp"} 
+                fill
+                alt=""
+                loading="eager"
+                ></Image>
+        </div>
+        <div className="logo_sticker">
+                <Image src={"/assets/landing/el_vueltero_sticker.webp"} 
+                fill
+                alt=""
+                loading="eager"
+                ></Image>
+        </div>
+        <div className="logo_grafitti">
+                <Image src={"/assets/landing/el_vueltero_seal.webp"} 
+                fill
+                alt=""
+                loading="eager"
+                ></Image>
+        </div>
+        <div className="esquinas top-left">
+                <Image src={"/assets/landing/corner.webp"} 
+                fill
+                alt=""
+                loading="eager"
+                ></Image>
+        </div>
+        <div className="esquinas bottom-left">
+                <Image src={"/assets/landing/corner.webp"} 
+                fill
+                alt=""
+                loading="eager"
+                ></Image>
+        </div>
+        <div className="esquinas top-right">
+                <Image src={"/assets/landing/corner.webp"} 
+                fill
+                alt=""
+                loading="eager"
+                ></Image>
+        </div>
+        <div className="esquinas bottom-right">
+                <Image src={"/assets/landing/corner.webp"} 
                 fill
                 alt=""
                 loading="eager"
